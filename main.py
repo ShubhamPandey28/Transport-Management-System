e359809e221a5347f174d3a99287334c846f7ed4
AshwinGinoria <ashwinginoria@gmail.com>
2020-05-08 20:51:38 +0530

    Improved Initial Structure of the code
    Signed-off-by: AshwinGinoria <ashwinginoria@gmail.com>

--- main.py
+++ main.py
@@ -2,41 +2,41 @@ __author__ = "Ashwin Ginoria"
 __email__ = "ashwinginoria@gmail.com"
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+
 from PyQt5.QtCore import Qt
-# import sqlite3
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
 
 
 # Subclass QMainWindow to customise your application's main window
 class MainWindow(QMainWindow):
-
-    # constructor for python
+    # Properties of the window like title goes here 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-
+        self.setGeometry(200, 200, 400, 300)
         self.setWindowTitle("SG Transport")
+        self.initUI()
 
-        label = QLabel("Created by Ashwin Ginoria and Shubham Pandey!! :)")
+    # All UI elements in Main-Window to be modified here
+    def initUI(self):
+        self.label = QLabel(self)
+        self.label.setText("Created by Ashwin Ginoria and Shubham Pandey!! :)")
+        # self.label.move(50, 50)
 
         # The `Qt` namespace has a lot of attributes to customise
         # widgets. See: http://doc.qt.io/qt-5/qt.html
-        label.setAlignment(Qt.AlignCenter)
+        self.label.setAlignment(Qt.AlignCenter)
 
         # Set the central widget of the Window. Widget will expand
         # to take up all the space in the window by default.
-        self.setCentralWidget(label)
+        self.setCentralWidget(self.label)
 
 
 def main():
+    # sys.argv provides with information about the OS for themeing
     app = QApplication(sys.argv)
-
-    # Creating a Window
-    window = MainWindow()
-    window.show()
-
-    # Executing the app
+    win = MainWindow()
+    win.show()
     sys.exit(app.exec_())
-
 
 if __name__ == '__main__':
     main()
