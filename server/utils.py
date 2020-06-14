9104aa5dbb2caddb25ff8d0b26eb519d8d73418b
ShubhamPandey28 <b18194@students.iitmandi.ac.in>
2020-06-14 22:18:11 +0530

    Updated SDK for multiple tables

--- server/utils.py
+++ server/utils.py
@@ -7,16 +7,36 @@ import requests
 
 url = 'http://127.0.0.1:5000/'
 
+def to_json(table_datas):
 
-def insert(data):
+    res = []
+
+    for table_data in table_datas:
+        res.append(json.dumps(table_data))
+
+    return res
+
+
+def insert(table_datas):
     
     '''
         Takes in the tuple as dictionary
         Inserting new values from ui to the server using the api
         raises error if unable to insert the tuple
+        
+        table_data : dict(table_name: string, value: dict )
+
+        value : the the row which is tobe inserted in table_name table.
+        
+        table_datas = list(table_data)
+
+        for eg. table_datas = [ 
+                                {'table_name':'cities', 'value': {'city_id':123, 'city_name':"'New York City'", 'city_state':"'New York'"}},
+                                {'table_name':'cities', 'value': {'city_id':321, 'city_name':"'Trenton'", 'city_state':"'New Jersey'"}}
+                            ]
     '''
 
-    response = requests.post(url, data=data)
+    response = requests.post(url, data={'data': to_json(table_datas)})
 
     if response.status_code == 202:
         return {'OK':True}
@@ -25,12 +45,12 @@ def insert(data):
         return {'OK':False, 'status_code': response.status_code,'reason':response.json()['reason']}
 
 
-def show():
+def show(table_name):
 
     '''
     As we have single table dummy DBMS just does `SELECT * FROM cities;`
     '''
-    response = requests.get(url)
+    response = requests.get(url, data = {'data':table_name})
     
     if response.status_code == 202:
         return {'OK':True,'result':response.json()['result']}
